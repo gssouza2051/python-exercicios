67d874823e3bac8c5c1d9ba80b5eedc9512607af
Gabriel Santos Souza <gssouza2051@gmail.com>
2022-03-19 11:48:01 -0300

    excluí

--- pacote-download/ex(1-100)/ex057.py
+++
@@ -1,6 +0,0 @@
-'''Faça um programa que leia o sexo de uma pessoa,
- mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
-n=str(input('Qual seu sexo? \n digite [M/F]:')).strip().upper()
-while n not in 'MmFf':
-    n=int(input('dados invalidos,digite novamente seu sexo ').strip().upper())
-print('sexo {} registrado com sucesso'.format(n))
--- pacote-download/ex(1-100)/ex077.py
+++
@@ -1,8 +0,0 @@
-'''Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
-Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.'''
-palavras= ('programar','aprender','grátis','estudo','praticar','mercado','python','linguagem','curso','futuro')
-for p in palavras:
-    print(f'\nNa palavra {p.upper()} temos ',end='')
-    for letra in p:
-        if letra.lower() in 'aáâãeéiou':
-            print(letra,end=' ')
