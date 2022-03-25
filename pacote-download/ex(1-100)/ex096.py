7d1e7815b1e6875c04312ef4a5560e093b29a617
Gabriel Santos Souza <gssouza2051@gmail.com>
2022-03-25 11:03:12 -0300

    exercicios atualizando

---
+++ pacote-download/ex(1-100)/ex096.py
@@ -0,0 +1,14 @@
+'''Faça um programa que tenha uma função chamada área(),
+ que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
+def área(larg,comp):
+    a=larg*comp
+    print(f'A área de um terreno {larg}X{comp} é de {a}m^2')
+
+
+print('Controle de terrenos')
+print('-'*20)
+l=float(input('Largura(m): '))
+c=float(input('Comprimento(m): '))
+área(l,c)
+
+
